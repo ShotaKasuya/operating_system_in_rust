bb70ba5074ff145dafbf0289642b47624b65d0d7
KasuyaShota <22rd055@ms.dendai.ac.jp>
2024-03-30 13:47:20 +0900

    Hello World Again
    「println」マクロ作成
    Writerの隠匿

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,20 @@
 
 mod vga_buffer;
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 
 static HELLO:&[u8]=b"Hello World!";
 #[no_mangle]
 pub extern "C" fn _start()->!{
-    let vga_buffer=0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize*2)=byte;
-            *vga_buffer.offset(i as isize*2+1)=0xb;
-        }
-    }
+    // println!("Hello World{}", "!");
+    panic!("some error");
 
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo)->!{
-    loop {
-
-    }
+    println!("{}", _info);
+    loop {}
 }
