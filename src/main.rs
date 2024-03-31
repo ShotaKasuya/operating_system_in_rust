e27613bd7e4cfde228ceee50e77bfc3c9283fdce
KasuyaShota <22rd055@ms.dendai.ac.jp>
2024-03-31 20:53:11 +0900

    テスト実装

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main="test_main"]
 
 mod vga_buffer;
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
 
 #[no_mangle]
-pub extern "C" fn _start()->!{
-    for i in 1..5 {
-        println!("Hello World{}", i);
-    }
-    panic!("some");
+pub extern "C" fn _start()->! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -20,4 +24,30 @@ pub extern "C" fn _start()->!{
 fn panic(_info: &PanicInfo)->!{
     println!("{}", _info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} test", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xF4);
+        port.write(exit_code as u32);
+    }
 }
