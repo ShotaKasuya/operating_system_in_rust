0e676a278b2969fc0f07f3e29db061ba08e9ab98
ShotaKasuya <22rd055@ms.dendai.ac.jp>
2024-04-20 12:02:32 +0900

    線形リストアロケーター1

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,3 +1,7 @@
+use core::mem;
+
+use crate::allocator::align_up;
+
 
 
 struct LinkedListAllocator {
@@ -17,13 +21,45 @@ impl LinkedListAllocator {
     /// 有効でヒープが未使用であることを保証しなければならないからである。
     /// このメソッドは一度しかよばれてはならない。
     pub unsafe fn init(&mut self, heap_start:usize, heap_size: usize) {
-        todo!()
+        self.add_free_region(heap_start, heap_size);
     }
 
     /// 与えられたメモリ領域をリストの先頭に追加する。
     unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
-        todo!()
+        // 解放された領域が`ListNode`を格納出来ることを確かめる
+        assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
+        assert!(size >= mem::size_of::<ListNode>());
+
+        // 新しいリストノードを作り、それをリストの先頭に追加する
+        let mut node = ListNode::new(size);
+        node.next = self.head.next.take();
+        let node_ptr = addr as *mut ListNode;
+        node_ptr.write(node);
+        self.head.next = Some(&mut *node_ptr);
     }
+
+    /// 与えられたサイズの解放された領域を探し、リストからそれを取り除く。
+    /// リストノードと割り当ての開始アドレスからなるタプルを返す。
+    fn find_region(&mut self, size: usize, align: usize)
+        -> Option<(&'static mut ListNode, usize)> {
+            // 現在のリストノードへの参照。繰り返すごとに更新していく
+            let mut current = &mut self.head;
+            // 連結リストから十分大きな領域を探す
+            while let Some(ref mut region) = current.next {
+                if let Ok(alloc_start) = Self::alloc_from_region(&region, size, align) {
+                    // 領域が割り当てに適している -> リストから除く
+                    let next = region.next.take();
+                    let ret = Some((current.next.take().unwrap(), alloc_start));
+                    current.next = next;
+                    return ret;
+                } else {
+                    // 割り当てに適していない -> 次の領域へ
+                    current = current.next.as_mut().unwrap();
+                }
+            }
+
+            None
+        }
 }
 
 struct ListNode {
