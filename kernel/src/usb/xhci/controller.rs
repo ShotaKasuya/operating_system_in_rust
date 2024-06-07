6a8519e383de4cbc61f220e6017545623877ca43
ShotaKasuya <22rd055@ms.dendai.ac.jp>
2024-06-07 19:02:36 +0900

    6/7

--- kernel/src/usb/xhci/controller.rs
+++ kernel/src/usb/xhci/controller.rs
@@ -1,18 +1,20 @@
+use bootloader_api::info::PixelFormat::U8;
 use volatile::Volatile;
 use crate::usb::xhci::registers::{CapabilityRegisters, OperationalRegisters};
 
 struct Controller {
     mmio_base: u64,
     cap_: Volatile<*const CapabilityRegisters>,
-    op_: Volatile<*const OperationalRegisters>,
+    op_: Volatile<*mut OperationalRegisters>,
     max_ports: u8,
 }
 
 impl Controller {
     pub fn new(mmio_base: u64) -> Self {
         let cap_ = mmio_base as *const CapabilityRegisters;
-        let op_ = unsafe { (mmio_base + (cap_.read().cap_length) as u64) as *const OperationalRegisters };
+        let op_ = unsafe { (mmio_base + (cap_.read().cap_length) as u64) as *mut OperationalRegisters };
         let max_ports = unsafe { cap_.read().hcsparams1.max_ports() };
+        
         Self {
             mmio_base,
             cap_: Volatile::new(cap_),
